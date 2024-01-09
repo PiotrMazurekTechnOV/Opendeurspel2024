1ad7797cd5ede77e60acc99b2ec954090831f112
Hasan <hasansahin.aksoy@leerling.kov.be>
2024-01-09 13:27:10 +0100

    woopsiesorry

--- admin/Admin_Opendeurspel/Admin_Opendeurspel/Form1.cs
+++ admin/Admin_Opendeurspel/Admin_Opendeurspel/Form1.cs
@@ -19,6 +19,8 @@ namespace Admin_Opendeurspel
             MessageBox.Show("Bong");
             MessageBox.Show("test123");
             MessageBox.Show("test321");
+            MessageBox.Show("HasanTest");
+
         }
     }
 }
