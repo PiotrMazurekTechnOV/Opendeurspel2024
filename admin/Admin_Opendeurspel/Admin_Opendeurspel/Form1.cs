f482fc0b5e9f25da59085ec14955b93e210266aa
PiotrMazurekTechnOV <93184377+PiotrMazurekTechnOV@users.noreply.github.com>
2024-01-09 13:14:27 +0100

    fixed merge issue

--- admin/Admin_Opendeurspel/Admin_Opendeurspel/Form1.cs
+++ admin/Admin_Opendeurspel/Admin_Opendeurspel/Form1.cs
@@ -15,11 +15,7 @@ namespace Admin_Opendeurspel
         public Form1()
         {
             InitializeComponent();
-<<<<<<< Updated upstream
             MessageBox.Show("test");
-=======
-            MessageBox.Show("Bong");
->>>>>>> Stashed changes
         }
     }
 }
