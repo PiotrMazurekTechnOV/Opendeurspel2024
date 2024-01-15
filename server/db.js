6cb8a82436b03d46bc7e24411ecc7c72b4f6e3e0
Nathaniel Goessens <ras.goe656@gmail.com>
2024-01-15 11:07:03 +0100

    FIxed errors

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "opendeurspel"
+  HOST : process.env.HOST,
+  USER :process.env.USER,
+  PASSWORD : process.env.USER,
+  DB : process.env.DB
   };
