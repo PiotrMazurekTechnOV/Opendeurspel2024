482b0d1dc24faf818758c3017cb5e70073a3342f
arjenkaekebeke <156067839+arjenkaekebeke@users.noreply.github.com>
2024-02-22 16:15:51 +0100

    endelijk werkt het

--- admin/Admin_Opendeurspel/Admin_Opendeurspel/Form2.Designer.cs
+++ admin/Admin_Opendeurspel/Admin_Opendeurspel/Form2.Designer.cs
@@ -33,8 +33,16 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.questionLbl = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -70,6 +78,7 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
@@ -92,11 +101,89 @@
             this.questionLbl.TabIndex = 4;
             this.questionLbl.Text = "AAAAA";
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(887, 464);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(418, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(401, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Location = new System.Drawing.Point(495, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button3.Location = new System.Drawing.Point(594, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(887, 464);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.questionLbl);
             this.Controls.Add(this.EnterBtn);
             this.Controls.Add(this.textBox1);
@@ -107,6 +194,7 @@
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,5 +206,12 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label questionLbl;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- admin/Admin_Opendeurspel/Admin_Opendeurspel/Form2.cs
+++ admin/Admin_Opendeurspel/Admin_Opendeurspel/Form2.cs
@@ -23,19 +23,39 @@ namespace Admin_Opendeurspel
             this.FormBorderStyle = FormBorderStyle.None;//verwijdert de borders
             this.Bounds = Screen.PrimaryScreen.Bounds;//zet het op de borders van jou scherm
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage ;
-            questionLbl.Text = "test";
-            questionLbl.Location = new Point(pictureBox2.Width/2, pictureBox2.Height/3);
+            questionLbl.Location = new Point(pictureBox3.Width*70/100, pictureBox3.Height*80/100);
+            button1. Location = new Point(pictureBox3.Width *55 / 100, pictureBox3.Height * 125 / 100);
+            button2.Location = new Point(pictureBox3.Width * 95 / 100, pictureBox3.Height * 125 / 100);
+            button3.Location = new Point(pictureBox3.Width * 140 / 100, pictureBox3.Height * 125 / 100);
+            questionLbl.Text = "hoeveel onderbroeken draagth John Doe";
+            button1.Text = "3"; button2.Text = "2"; button3.Text = "1";
             questionLbl.Font = new Font("Calibri", 18);
+            button1.Font = new Font("Calibri", 18);
+            button2.Font = new Font("Calibri", 18);
+            button3.Font = new Font("Calibri", 18);
             questionLbl.AutoSize = true;
+            button1.Size = new System.Drawing.Size(200, 90); 
+            button2.Size = new System.Drawing.Size(200, 90); 
+            button3.Size = new System.Drawing.Size(200, 90); 
+            button1.BringToFront();
+            button2.BringToFront();
+            button3.BringToFront();
             this.questionLbl.Parent = this.pictureBox2;
+            this.button1.Parent = this.pictureBox2;
+            this.button2.Parent = this.pictureBox2;
+            this.button3.Parent = this.pictureBox2;
             this.questionLbl.BackColor = Color.Transparent;
+            
             questionLbl.Padding = new Padding(6);
+            button1.Padding = new Padding(6);
+            button2.Padding = new Padding(6);
+            button3.Padding = new Padding(6);
             this.Controls.Add(questionLbl);
-            questionLbl.Location = new Point(pictureBox2.Width / 2, pictureBox2.Height / 3);
             questionLbl.BringToFront();
-
-
-
+            questionLbl.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.Visible = false;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -50,7 +70,45 @@ namespace Admin_Opendeurspel
                 pictureBox1.Visible = false;
                 textBox1.Visible = false;
                 EnterBtn.Visible = false;
+                pictureBox2.Visible = true;
+                questionLbl.Visible = true;
             }
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Juist Antwoord");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fout Antwoord");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fout Antwoord");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fout Antwoord");
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fout Antwoord");
+
         }
     }
 }
