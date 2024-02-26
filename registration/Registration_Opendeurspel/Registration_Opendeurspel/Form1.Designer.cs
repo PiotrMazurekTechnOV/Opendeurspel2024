cce5f03fa290817fbcaad1e576b615c0f26fc30a
Ya33tech <156068165+Ya33tech@users.noreply.github.com>
2024-02-26 12:04:27 +0100

    CHANGES

--- registration/Registration_Opendeurspel/Registration_Opendeurspel/Form1.Designer.cs
+++ registration/Registration_Opendeurspel/Registration_Opendeurspel/Form1.Designer.cs
@@ -31,6 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.InschrijfBtn = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // InschrijfBtn
@@ -43,11 +48,62 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(286, 82);
+            this.textBox1.Location = new System.Drawing.Point(286, 299);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(229, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Wat zijn uw Interesses op deze school";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(288, 81);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Voornaam";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 134);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Familienaam";
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(288, 188);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(229, 22);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "Leeftijd";
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(286, 236);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(229, 22);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "Email";
+            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(255, 421);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -56,6 +112,11 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.InschrijfBtn);
             this.DoubleBuffered = true;
@@ -71,6 +132,11 @@
 
         private System.Windows.Forms.Button InschrijfBtn;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- registration/Registration_Opendeurspel/Registration_Opendeurspel/Form1.cs
+++ registration/Registration_Opendeurspel/Registration_Opendeurspel/Form1.cs
@@ -26,8 +26,8 @@ namespace Registration_Opendeurspel
             InschrijfBtn.Cursor = Cursors.Hand;
             InschrijfBtn.FlatStyle = FlatStyle.Flat;
             InschrijfBtn.BackColor = Color.Transparent;
-            //InschrijfBtn.FlatAppearance.BorderSize = 0;
-            InschrijfBtn.ForeColor = Color.Red;
+            InschrijfBtn.FlatAppearance.BorderSize = 0;
+            InschrijfBtn.ForeColor = Color.Transparent;
             InschrijfBtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
             InschrijfBtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
             int percentage = 112;
@@ -36,18 +36,84 @@ namespace Registration_Opendeurspel
             int parentHeight = this.ClientSize.Height;
             int newX = (parentWidth * percentage) / 100;
             int newY = (parentHeight * percentage2) / 100;
-            InschrijfBtn.Location = new Point(newX, newY);
+            this.InschrijfBtn.Location = new Point(newX, newY);
+
             this.WindowState = FormWindowState.Maximized;
             this.FormBorderStyle = FormBorderStyle.None;
             this.Bounds = Screen.PrimaryScreen.Bounds;
             this.BackgroundImageLayout = ImageLayout.Stretch;
             this.textBox1.AutoSize = false;
-            this.textBox1.Size = new System.Drawing.Size(475, 155);
+            this.textBox1.Size = new Size(475, 155);
             int percentage3 = 90;
             int percentage4 = 140;
             int newX1 = (parentWidth * percentage3) / 100;
             int newY1 = (parentHeight * percentage4) / 100;
             this.textBox1.Location = new Point(newX1, newY1);
+
+            this.textBox2.AutoSize = false;
+            this.textBox2.Size = new Size(475, 75);
+            int percentage5 = 90;
+            int percentage6 = 37;
+            int newX2 = (parentWidth * percentage5) / 100;
+            int newY2 = (parentHeight * percentage6) / 100;
+            this.textBox2.Location = new Point(newX2, newY2);
+
+            this.textBox3.AutoSize = false;
+            this.textBox3.Size = new Size(475, 75);
+            int percentage7 = 90;
+            int percentage8 = 62;
+            int newX3 = (parentWidth * percentage7) / 100;
+            int newY3 = (parentHeight * percentage8) / 100;
+            this.textBox3.Location = new Point(newX3, newY3);
+
+            this.textBox4.AutoSize = false;
+            this.textBox4.Size = new Size(475, 75);
+            int percentage9 = 90;
+            int percentage10 = 88;
+            int newX4 = (parentWidth * percentage9) / 100;
+            int newY4 = (parentHeight * percentage10) / 100;
+            this.textBox4.Location = new Point(newX4, newY4);
+
+            this.textBox5.AutoSize = false;
+            this.textBox5.Size = new Size(475, 75);
+            int percentage11 = 90;
+            int percentage12 = 115;
+            int newX5 = (parentWidth * percentage11) / 100;
+            int newY5 = (parentHeight * percentage12) / 100;
+            this.textBox5.Location = new Point(newX5, newY5);
+
+            this.checkBox1.AutoSize = false;
+            int percentage13 = 86;
+            int percentage14 = 213;
+            int newX6 = (parentWidth * percentage13) / 100;
+            int newY6 = (parentHeight * percentage14) / 100;
+            this.checkBox1.Location = new Point(newX6, newY6);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty;
+        }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = string.Empty;
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = string.Empty;
+        }
+
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = string.Empty;
         }
     }
 }
