20a957d2ee1f12a81d26f85dc8d47d5f589b0189
PiotrMazurekTechnOV <93184377+PiotrMazurekTechnOV@users.noreply.github.com>
2024-02-27 10:42:22 +0100

    changed location of question project

--- question/Question_Opendeurspel/Question_Opendeurspel/Form1.Designer.cs
+++ question/Question_Opendeurspel/Question_Opendeurspel/Form1.Designer.cs
@@ -28,107 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroTextBox1 = new ReaLTaiizor.Controls.MetroTextBox();
-            this.lokaalTxb = new ReaLTaiizor.Controls.NightTextBox();
-            this.EnterBtn = new ReaLTaiizor.Controls.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.enterBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // metroTextBox1
+            // textBox1
             // 
-            this.metroTextBox1.AutoCompleteCustomSource = null;
-            this.metroTextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
-            this.metroTextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
-            this.metroTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.metroTextBox1.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
-            this.metroTextBox1.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.metroTextBox1.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
-            this.metroTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.metroTextBox1.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.metroTextBox1.Image = null;
-            this.metroTextBox1.IsDerivedStyle = true;
-            this.metroTextBox1.Lines = null;
-            this.metroTextBox1.Location = new System.Drawing.Point(256, 141);
-            this.metroTextBox1.MaxLength = 32767;
-            this.metroTextBox1.Multiline = false;
-            this.metroTextBox1.Name = "metroTextBox1";
-            this.metroTextBox1.ReadOnly = false;
-            this.metroTextBox1.Size = new System.Drawing.Size(277, 39);
-            this.metroTextBox1.Style = ReaLTaiizor.Enum.Metro.Style.Light;
-            this.metroTextBox1.StyleManager = null;
-            this.metroTextBox1.TabIndex = 0;
-            this.metroTextBox1.Text = "In welk lokaal zitten jullie?";
-            this.metroTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.metroTextBox1.ThemeAuthor = "Taiizor";
-            this.metroTextBox1.ThemeName = "MetroLight";
-            this.metroTextBox1.UseSystemPasswordChar = false;
-            this.metroTextBox1.WatermarkText = "";
+            this.textBox1.Location = new System.Drawing.Point(324, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // lokaalTxb
+            // label1
             // 
-            this.lokaalTxb.ActiveBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(93)))), ((int)(((byte)(89)))));
-            this.lokaalTxb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(48)))), ((int)(((byte)(67)))));
-            this.lokaalTxb.BaseBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(48)))), ((int)(((byte)(67)))));
-            this.lokaalTxb.ColorBordersOnEnter = true;
-            this.lokaalTxb.DisableBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
-            this.lokaalTxb.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.lokaalTxb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(131)))), ((int)(((byte)(140)))));
-            this.lokaalTxb.Image = null;
-            this.lokaalTxb.Location = new System.Drawing.Point(316, 186);
-            this.lokaalTxb.MaxLength = 32767;
-            this.lokaalTxb.Multiline = false;
-            this.lokaalTxb.Name = "lokaalTxb";
-            this.lokaalTxb.ReadOnly = false;
-            this.lokaalTxb.ShortcutsEnabled = true;
-            this.lokaalTxb.ShowBottomBorder = true;
-            this.lokaalTxb.ShowTopBorder = true;
-            this.lokaalTxb.Size = new System.Drawing.Size(145, 59);
-            this.lokaalTxb.TabIndex = 0;
-            this.lokaalTxb.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
-            this.lokaalTxb.UseSystemPasswordChar = false;
-            this.lokaalTxb.Watermark = "";
-            this.lokaalTxb.WatermarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(120)))), ((int)(((byte)(129)))));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(307, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "In welke lokaal zit jij";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // EnterBtn
+            // enterBtn
             // 
-            this.EnterBtn.BackColor = System.Drawing.Color.Transparent;
-            this.EnterBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.EnterBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.EnterBtn.EnteredBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.EnterBtn.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.EnterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.EnterBtn.Image = null;
-            this.EnterBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.EnterBtn.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.EnterBtn.Location = new System.Drawing.Point(329, 271);
-            this.EnterBtn.Name = "EnterBtn";
-            this.EnterBtn.PressedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.EnterBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.EnterBtn.Size = new System.Drawing.Size(120, 40);
-            this.EnterBtn.TabIndex = 1;
-            this.EnterBtn.Text = "Enter";
-            this.EnterBtn.TextAlignment = System.Drawing.StringAlignment.Center;
-            this.EnterBtn.Click += new System.EventHandler(this.button1_Click);
+            this.enterBtn.Location = new System.Drawing.Point(294, 230);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(172, 59);
+            this.enterBtn.TabIndex = 0;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.EnterBtn);
-            this.Controls.Add(this.lokaalTxb);
-            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private ReaLTaiizor.Controls.MetroTextBox metroTextBox1;
-        private ReaLTaiizor.Controls.NightTextBox lokaalTxb;
-        private ReaLTaiizor.Controls.Button EnterBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button enterBtn;
     }
 }
 
--- question/Question_Opendeurspel/Question_Opendeurspel/Form1.cs
+++ question/Question_Opendeurspel/Question_Opendeurspel/Form1.cs
@@ -1,44 +1,80 @@
-﻿using ReaLTaiizor.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace Question_Opendeurspel
 {
     public partial class Form1 : Form
     {
+        private HttpClient client;
         public Form1()
         {
             InitializeComponent();
-
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://192.168.0.144/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Minimized;//zet het fullscreen
-            this.FormBorderStyle = FormBorderStyle.None;//verwijdert de borders
-            this.Bounds = Screen.PrimaryScreen.Bounds;//zet het op de borders van jou scherm
-            
-
         }
 
-    
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            if (lokaalTxb.Text == "112")
+        }
+
+        private async void enterBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
             {
-                Form2 form2 = new Form2();
-                form2.Show();
-                Hide();
-                this.Closed += (s, args) => Close();
+                MessageBox.Show("Vul een klaslokaal in.");
+            }
+            else
+            {
+                try
+                {
+                    Location location = await GetLocationByRoom();
+                    Form2 form2 = new Form2(location);
+                    form2.Show();
+                    Hide();
+                    this.Closed += (s, args) => Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
+
+        private async Task<Location> GetLocationByRoom()
+        {
+            int room = Convert.ToInt32(textBox1.Text);
+            var res = await client.GetAsync("location/" + room);
+
+            var jsonResponse = await res.Content.ReadAsStringAsync();
+            MessageBox.Show(jsonResponse);
+            Location location = JsonConvert.DeserializeObject<Location>(jsonResponse);
+
+            return location;
+        }
+    }
+    public class Location
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string room { get; set; }
+
     }
 }
